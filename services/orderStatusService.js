97b7c10ac0b4ee83996cfc804a65f791ca3579d4
Ankit <knjangir24.com>
2025-04-10 12:16:32 +0530

    Order_Module_Updates

--- services/orderStatusService.js
+++ services/orderStatusService.js
@@ -2,28 +2,39 @@ const orderModel = require("../models/order.model");
 
 const orderStatusService = async (req, res) => {
   try {
-    const {From, To , status} = req.query
+    const { From, To, status } = req.query;
     const startDate = new Date(From);
     const endDate = new Date(To);
-
+    let data;
     endDate.setHours(23, 59, 59, 999);
-
-    const data = await orderModel.find({
-      status: status,
-      createdAt: {
-        $gte: startDate,
-        $lte: endDate,
-      },
+    if (status === "all") {
+      data = await orderModel.find({
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      });
+    } else {
+      data = await orderModel.find({
+        status: status,
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      });
+    }
+    let Revenue = 0;
+    let count = 0;
+    data.forEach((element) => {
+      Revenue = Revenue + element.totalPrice;
+      count++;
     });
-    let Revenue = 0
-     data.forEach((element)=>{
-      Revenue = Revenue + element.totalPrice
-    })
 
-    console.log(Revenue)
+    // console.log(Revenue)
     res.status(200).send({
       status: 200,
-      totalRevenue:Revenue,
+      Total: count,
+      totalRevenue: Revenue,
       filterData: data,
     });
   } catch (error) {
